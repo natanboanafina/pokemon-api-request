ffd8b95ba12183b7227092544b744dc5c6d4d603
Natan Boanafina <boanafina.natan@gmail.com>
2021-12-10 15:23:55 -0300

    Project updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', ()=>{
-  console.log("Olá, mundo!");
   fetchingApi();
 })
 
@@ -7,10 +6,17 @@ let searchingPokemon = document.querySelector('#searching');
 let newResults = document.querySelector('.results')
 
 async function fetchingApi(eventReq){
+  try{
   const res = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${eventReq}`)
   const data = await res.json();
-
   renderFilt(data)
+}catch(e){
+  if(e === 400 || e === 404){
+    console.log("Página não encontrada");
+  }
+  console.log(e + ' Erro na API');
+}
+
 }
 searchingPokemon.addEventListener('input', (event) =>{
   let eventReq = event.target.value.trim().toLowerCase();
@@ -20,24 +26,24 @@ searchingPokemon.addEventListener('input', (event) =>{
 //   console.log(name);
 // }
 function renderFilt(data){
+let newData = data.data;
 let output = '';
-  data.data.filter((item) => {
+newData.filter((item) => {
   output += `
     <ul class="bg-dark bg-gradient border border-info" style="--bs-bg-opacity: .2;">
-      <li >Nome: ${item.name}</li>
-      <li >Supertipo: ${item.supertype}</li>
-      <li >Tipo: ${item.types}</li>
-      <li >Subtipo: ${item.subtypes}</li>
-      <li >Evolução de: ${item.evolvesFrom}
-    </li>
-      <li >Ataques: ${item.attacks.map(attack => 
+      <li>ID: ${item.id}</li>
+      <li>Nome: ${item.name}</li>
+      <li>Supertipo: ${item.supertype}</li>
+      <li>Tipo: ${item.types}</li>
+      <li>Subtipo: ${item.subtypes}</li>
+      <li>Evolução de: ${item.evolvesFrom}</li>
+      <li>Nome(s) do(s) Ataque(s): ${item.attacks.map(att => `${att.name}`)}</li>
+      <li>Custo do(s) Ataque(s): ${item.attacks.map(attCost => `${attCost.cost}`)}
+      <figure>
+       <img src="${item.images.small}" alt="Logo">
+      </figure>
+    </ul>
         `
-        <span>${attack.name}</span>,
-        <span>${attack.cost}</span>
-        `)}</li>
-
-      </ul>
-  `
   })
   newResults.innerHTML = output;
 }
